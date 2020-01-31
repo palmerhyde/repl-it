848217208daadbeb6e0b676b610c499d66d521c6
Liam Molloy <liam_j_molloy@hotmail.com>
2020-01-31 21:36:14 +0000

    supportedLeagues functionality.

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-// This is a test. Does it work? Do I need to check in?
-console.log('hello world!')
+// TODO: get from config
+supportedLeagues = [
+  'Spain-LaLiga-2019-2020',
+  'Germany-Bundesliga-2019-2020',
+  'England-Premier-League-2019-2020'
+]
+
+const matches = [
+  'https://www.whoscored.com/Matches/1388411/Live/Germany-Bundesliga-2019-2020-Hertha-Berlin-Schalke-04',
+  'https://www.whoscored.com/Matches/1401668/Live/Argentina-Superliga-2019-2020-Velez-Sarsfield-Aldosivi',
+  'https://www.whoscored.com/Matches/1376114/Live/England-Premier-League-2019-2020-Brighton-Chelsea',
+  'https://www.whoscored.com/Matches/1394275/Live/Spain-LaLiga-2019-2020-Getafe-Real-Madrid',
+  'https://www.whoscored.com/Matches/1381664/Live/England-Championship-2019-2020-Brentford-Nottingham-Forest'
+]
+
+const yesterdaysMatches = matches.filter(match => isSupportedLeague(match))
+
+console.log(yesterdaysMatches)
+
+function isSupportedLeague(match) {
+  console.log(match)
+  for (let league in supportedLeagues) {
+    if (match.includes(supportedLeagues[league])) {
+      return true
+    }
+  }
+
+  return false
+}
